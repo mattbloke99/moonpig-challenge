cb91dffbd33e82cde35b97440d41411a2ee73a9d
Martin Perceval <martin.perceval@smartfocus.com>
2016-12-15 11:01:42 +0000

    Added tests for utils.js

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,13 +1,13 @@
+export function isEmpty(val) {
+    return (val == undefined || val == null || val.length <= 0) ? true : false;
+};
+
 export function truncate(string, length) {
   if (isEmpty(string)) {
     return '';
   }
   if (string.length > length) {
-     return string.substring(0,length)+'...';
-   }
-   return string;
-};
-
-export function isEmpty(val) {
-    return (val == undefined || val == null || val.length <= 0) ? true : false;
+    return string.substring(0,length)+'...';
+  }
+  return string;
 };
